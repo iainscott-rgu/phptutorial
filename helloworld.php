1f07c25e960189458de9c7494da1047e8c88949f
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-15 09:41:34 +0000

    go go go

--- helloworld.php
+++ helloworld.php
@@ -8,7 +8,7 @@ echo $text;
 echo $sp;
 echo $moretext;
 
-$modules = array("My First Array 1", "My First Array 2", "My First Array 3","Ok... enough of the Arrays");
+$modules = array("My First Array 1", "My First Array 2", "My First Array 3","Ok... enough of the Arrays",);
 
 echo $modules;
 
