06b947b441d3726d0974b081f235d2b23d6505f7
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-15 10:21:31 +0000

    go go go

--- helloworld.php
+++ helloworld.php
@@ -1,34 +1,74 @@
-<?php
+<html>
+<head>
 
-$text = "Hello World";
-$sp = " ";
-$moretext = "You Suck!";
+    <?php
 
-echo $text;
-echo $sp;
-echo $moretext;
+    /*
 
-$modules = array('a' => 'big time');
+    $text = "Hello World";
+    $sp = " ";
+    $moretext = "You Suck!";
 
-echo " Yeh {$modules['a']}!!!";
+    echo $text;
+    echo $sp;
+    echo $moretext;
 
-$name = "<p>Fanny Pants";
-$age = 10000;
-$str = " years old!</p>";
+    $modules = array('a' => 'big time');
 
-echo $name. " is ";
-echo $age;
-echo $str;
+    echo " Yeh {$modules['a']}!!!";
+
+    $name = "<p>Fanny Pants";
+    $age = 10000;
+    $str = " years old!</p>";
+
+    echo $name. " is ";
+    echo $age;
+    echo $str;
 
 
-if($name == "Dude") {
+    if($name == "Dude") {
 
-    print "Hey man, I kow you don't I???";
-}
+        print "Hey man, I kow you don't I???";
+    }
 
-else {
+    else {
 
-    print "hey screw you man!!!!";
-}
+        print "hey screw you man!!!!";
+    }
 
-?>
+    */
+
+    $myage = 25;
+
+    ?>
+
+
+</head>
+<header>
+
+    <h1>Aberdeen is	the	home of	specs, mugs	and	sausage	rolls.</h1>
+
+    <p>You are only	allowed	to:</p>
+    <ul><li>Buy specs if	you	are	over 16</li>
+    <li>Buy mugs if	you	are	over 18</li>
+    <li>Buy sausage rolls if	you	are	over 21</li>
+    </ul>
+
+
+
+
+</header>
+<body>
+
+
+<main>
+
+
+</main>
+<footer>
+
+
+
+</footer>
+</body>
+</html>
