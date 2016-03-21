2f4ab1fe16629571a78f009e8be8193b10a3e18f
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-21 14:55:32 +0000

    go! go! go!

--- loops.php
+++ loops.php
@@ -1,13 +1,28 @@
 <?php
 
-
 for($i=1; $i < 30; $i++){
 
-    echo "On day". $i."the following items are made.";
+    echo "On day". $i."the following items are available:";
 
     if($i % 2 == 0){
 
+        echo "Mugs";
+        $y = 1;
     }
+
+    if($i % 3 == 0){
+
+        echo "hugs";
+        $y = 1;
+    }
+
+    if($ % 4 == 0){
+
+        echo "tugs";
+        $y = 1;
+    }
+
+    echo "<p>";
 }
 
 ?>
