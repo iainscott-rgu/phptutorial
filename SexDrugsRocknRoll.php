c37eb38d18ee5301a20ae182b741dce173c8cf6b
Iain Scott <iainscott-rgu>
2016-03-21 10:36:32 +0000

    go go go!

--- SexDrugsRocknRoll.php
+++ SexDrugsRocknRoll.php
@@ -3,22 +3,22 @@
 
        <title>Sex drugs and Rock'n'Roll</title>
 
-       <?PHP
-
-       $provisionedActivities = array("Specs","Drugs","Rock and Roll");
-
-
-       foreach($provisionedActivities as $x) {
-           print "<p>$x</p>";
-       }
-       ?>
-
-
    </head>
 
-    <body>
+<body>
 
 My Web Page...
-    </body>
+
+<?PHP
+
+$provisionedActivities = array("Specs","Drugs","Rock and Roll");
+
+foreach($provisionedActivities as $x) {
+    print "<p>$x</p>";
+}
+
+?>
+
+</body>
 
 </html>
