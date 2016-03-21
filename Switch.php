5966c18ec47296682dfb31261454f56e3da493c8
Iain Scott <iainscott-rgu>
2016-03-21 10:24:55 +0000

    go go go!

---
+++ Switch.php
@@ -0,0 +1,22 @@
+<?php
+
+$wantedgoods = "mugs";
+
+switch ($wantedgoods){
+
+    case mugs:
+        echo "You have to be 18 to buy mugs";
+        break;
+    case specs:
+        echo "You have to be 16 to buy spacs";
+        break;
+    case rolls:
+        echo "You have to be 21 to buy rolls";
+        break;
+    default: "what have you done???";
+
+}
+
+
+
+?>
