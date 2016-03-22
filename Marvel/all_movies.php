182dc5bd922a14b58f5e654a222cffb72304e8da
Iain Scott <odyssey3007@hotmail.com>
2016-03-22 21:49:52 +0000

    go go gadget arms!!

--- Marvel/all_movies.php
+++ Marvel/all_movies.php
@@ -5,7 +5,7 @@ include ("connection.php");
 $sql="SELECT * FROM marvelmovies";
 
 $result=mysqli_query($db,$sql);
-while($row = $result->fetch_assoc())
+while($row = $result->fetch_array())
 {
 echo "this is the title".['title'];
 
