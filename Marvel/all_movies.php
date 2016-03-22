0575d61f3506c92d0a682b66bb05c49cb97aa6f1
Iain Scott <odyssey3007@hotmail.com>
2016-03-22 22:02:35 +0000

    go go gadget arms!!

--- Marvel/all_movies.php
+++ Marvel/all_movies.php
@@ -9,7 +9,7 @@ $sql="SELECT * FROM marvelmovies";
 $result=mysqli_query($db,$sql);
 while($row = $result->fetch_array())
 {
-echo "<p>".$row['marvelMovieID'].$row['title'].$row['yearReleased'].$row['productionStudio'].$row['notes']."<br>";
+echo "<p>".$row['marvelMovieID']." ".$row['title']." ".$row['yearReleased']." ".$row['productionStudio']." ".$row['notes']."<br>";
 
 }
 
